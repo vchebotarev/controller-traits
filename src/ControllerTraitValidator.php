28256c42a5260d983db681589cb1a2f2e2d80220
vchebotarev <v.yu.chebotarev@mail.ru>
2019-12-16 18:26:50 +0300

    validator trait

---
+++ src/ControllerTraitValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chebur\ControllerTraits;
+
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+trait ControllerTraitValidator
+{
+    /**
+     * @var ValidatorInterface
+     */
+    private $traitValidator;
+
+    /**
+     * @required
+     * @internal
+     */
+    public function setTraitValidator(ValidatorInterface $traitValidator) : void
+    {
+        $this->traitValidator = $traitValidator;
+    }
+
+    public function validate($value, $constraints = null, $groups = null): ConstraintViolationListInterface
+    {
+        return $this->traitValidator->validate($value, $constraints, $groups);
+    }
+}
